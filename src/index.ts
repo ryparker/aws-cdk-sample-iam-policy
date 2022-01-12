13da4d61076be3dfe1aa5a60cd81958fd88224e0
Ryan Parker <ryan.parker3@outlook.com>
2022-01-12 09:33:15 -0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
-import { App, Stack } from 'aws-cdk-lib';
+import { App, Stack, CfnOutput } from "aws-cdk-lib";
+import { Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 const app = new App();
-const stack = new Stack(app, 'sample');
+const stack = new Stack(app, "sample-iam-policy");
+const policy = new Policy(stack, "policy", {
+  force: true,
+  statements: [
+    new PolicyStatement({
+      actions: ["s3:ListBucket"],
+      resources: ["arn:aws:s3:::*"],
+    }),
+  ],
+});
+
+// new CfnOutput(stack, "Policy", {
+//   value: policy.policyName,
+// });
